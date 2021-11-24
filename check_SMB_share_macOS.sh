e18dd0239eba043039fc8e01de1c5db2ac273c7f
glmonarch <glmonarch@users.noreply.github.com>
2021-11-24 19:05:08 +0300

    Update check_SMB_share_macOS.sh

--- check_SMB_share_macOS.sh
+++ check_SMB_share_macOS.sh
@@ -4,41 +4,50 @@
 function mountSHARE {
 echo ""
 echo "Unmount SMB share..."
-umount /Users/admin/Desktop/mnt1
+/sbin/umount /Users/admin/Desktop/mnt1
 echo ""
 echo "Try to mount SMB share..."
 sleep 3
-mount -t smbfs //viewer:viewer@192.168.45.104/share /Users/admin/Desktop/mnt1
+/sbin/mount -t smbfs //viewer:viewer@192.168.44.5/share_settings /Users/admin/Desktop/mnt1
 if [ $? -eq 0 ]; then
   echo "SMB share succesfully mounted!"
 else
   echo "Error mounting SMB share!"
+  exit 0
 fi
+
 }
 
 
 # Function test READ from SMB share
 function testREAD {
 echo "Start checking READ access..."
-contentShare=$(cat /Users/admin/Desktop/mnt1/do_not_delete)
-echo "$contentShare"
-if [ "$contentShare" = 123 ]; then
-  echo "READ from SMB share OK!"
-  echo ""
-  echo "OK" > ./test_share_result.log
+cat /Users/admin/Desktop/mnt1/do_not_delete & pid=$!;
+{ sleep 3; kill "$pid"; }
+if [ $? -ne 0 ]; then
+  contentShare=$(cat /Users/admin/Desktop/mnt1/do_not_delete)
+    if [ "$contentShare" = QAZ123wsx456EDC ]; then
+      echo "READ from SMB share OK!"
+      echo ""
+      echo "OK" > /tmp/SMB_share_test_result.log
+    else
+      echo "READ from SMB share FAILED!"
+      echo "FAIL" > /tmp/SMB_share_test_result.log
+    fi
 else
   echo "READ from SMB share FAILED!"
-  echo "FAIL" > ./test_share_result.log
+  echo "FAIL" > /tmp/SMB_share_test_result.log
 fi
+
 }
 
 
 # SMB share already mounted check
-isMounted=`mount | grep 192.168.45.104/share`
+isMounted=`/sbin/mount | grep 192.168.44.5/share_settings`
 
 if [ -n "$isMounted" ]; then
   echo ""
-  echo "SMB share already mounted"
+  echo "SMB share probably already mounted..."
   echo ""
   testREAD
 elif [ -z "$isMounted" ]; then
